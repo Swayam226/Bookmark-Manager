ed45c4601734f3f5ba401eae68e352c22a478d8a
Swayam <swayamkumar226@gmail.com>
2025-05-13 20:37:30 +0530

    all done ig

--- script.js
+++ script.js
@@ -4,31 +4,70 @@ const bkurl = document.getElementById("bkurl")
 const btn = document.getElementById("btn")
 const savedbookmark = document.getElementById("savedbmpara")
 
+// getitem returns a string
+// parse converts into an array of objects
 
 
+function displaybookmarks() {
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 
-// load bookmarks
+    savedbookmark.innerHTML = "";
+
+    // showing all bookmarks
+    bookmarks.forEach((bookmark, index) => {
+        const div = document.createElement("div");
+        div.className = "items";
+
+        div.innerHTML = `<strong>${index + 1}. ${bookmark.bname}</strong> - <a href="${bookmark.burl}" target="_blank">${bookmark.burl}</a><br><br>`;
+
+        savedbookmark.appendChild(div);
+    });
+
+    if (bookmarks.length === 0) {
+        savedbookmark.textContent = "No Bookmarks Found"
+    }
+}
+
+// loading bookmarks on page load
 window.addEventListener("DOMContentLoaded", () => {
-    const storedbk = localStorage.getItem("bookmark");
-
-    if (storedbk) {
-        const bookmark = JSON.parse(storedbk);
-        savedbookmark.innerHTML = `${bookmark.bname} - <a href="${bookmark.burl}" target="_blank">${bookmark.burl}</a>`;
-    }
-    else {
-        savedbookmark.textContent = "Sorry, no bookmark found :(";
-    }
+    displaybookmarks();
 })
 
-// save bookmarks
+// save bookmarks 
 btn.addEventListener("click", () => {
-    const bookmark = {
-        // used object to store multiple values
+    const newbookmark = {
         bname: bkname.value.trim(),
         burl: bkurl.value.trim()
     }
-    localStorage.setItem("bookmark", JSON.stringify(bookmark));
-    savedbookmark.innerHTML = `${bookmark.bname} - <a href="${bookmark.burl}" target="_blank">${bookmark.burl}</a>`;
 
+    if (!newbookmark.bname || !newbookmark.burl) {
+        alert("Please enter both name and URL.");
+        return;
+    }
+
+    const bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+
+    bookmarks.push(newbookmark);
+
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+    bkname.value = "";
+    bkurl.value = "";
+
+    displaybookmarks();
 })
+
+// clear bookmarks
+
+const clearbtn = document.getElementById("clearbtn");
+
+clearbtn.addEventListener('click', () => {
+    localStorage.removeItem("bookmarks");
+
+    savedbookmark.innerHTML = "";
+
+    savedbookmark.textContent = "All Empty :("
+})
+
+
 
